3ad308669fc0e106494e3e3c2971667b8f27a5c2
TejasLamba2006 <tejas22feb@gmail.com>
2026-06-08 16:22:02 +0530

    feat: add export script to convert YOLOv8 model to TFLite format and include best.onnx weights

--- scripts/export_saved.py
+++ scripts/export_saved.py
@@ -1,8 +1,9 @@
 from ultralytics import YOLO
 
 model = YOLO("runs/detect/train-5/weights/best.pt")
-
 model.export(
-    format="saved_model",
-    imgsz=320
+    format="tflite",
+    imgsz=320,
+    int8=True,        
+    data="data.yaml",   
 )
