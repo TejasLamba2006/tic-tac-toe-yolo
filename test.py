337f22a23c24a0e231b0f7e1e7741052af41528a
TejasLamba2006 <tejas22feb@gmail.com>
2026-06-08 16:27:15 +0530

    refactor: format tflite interpreter initialization for improved readability

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import tflite_runtime.interpreter as tflite
 import numpy as np
 import cv2
 
-interp = tflite.Interpreter('runs/detect/train-5/weights/best_saved_model/best_int8.tflite')
+interp = tflite.Interpreter(
+    'runs/detect/train-5/weights/best_saved_model/best_int8.tflite')
 interp.allocate_tensors()
 print('Input:', interp.get_input_details()[0])
 print('Output:', interp.get_output_details()[0])
-"
